d87d6ce6a3c2515922f54236de4fa0afea39c33a
machadinhacega <izav.machado@gmail.com>
2020-10-14 14:20:52 -0300

    Deixa marcar um local no mapa

---
+++ files/scripts/page-create-orphanege.js
@@ -0,0 +1,34 @@
+// create map
+const map = L.map("mapid").setView([-3.7428317,-38.5422904], 15);
+
+
+// create and add titleLayer
+L
+.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+.addTo(map);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./files/assets/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29, 68],
+})
+
+// marcando a variavel como let permite q possa alterar futuramente, diferente da const
+// iniciando a variavel vazia para modificar futuramente
+let marker;
+
+// create and add marker
+// função do leaflet: on. O event retorna uma longitude e uma latitude
+map.on('click', function(event){
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng
+
+    // remove icon
+    // se o marker existir ele vai para o map.remove
+    marker && map.removeLayer(marker)
+
+    // add icon layer
+    marker = L.marker([lat, lng], {icon})
+    .addTo(map)
+})
